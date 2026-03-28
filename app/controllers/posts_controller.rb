06f4d5dbe5eaa554562e146bd0f7ecdb72284e42
scrollloack <43851010+scrollloack@users.noreply.github.com>
2026-03-28 18:38:27 +0800

    Add post feature with turbo

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,11 +25,9 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: "Post was successfully created." }
-        format.json { render :show, status: :created, location: @post }
+        format.turbo_stream
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
       end
     end
   end
